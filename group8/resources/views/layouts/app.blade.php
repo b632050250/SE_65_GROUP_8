cc9ec6c80a53237ddc886e56e607645f43f7f321
b632050250 <78350857+b632050250@users.noreply.github.com>
2023-03-26 20:01:48 +0700

    update2

--- group8/resources/views/layouts/app.blade.php
+++ group8/resources/views/layouts/app.blade.php
@@ -59,7 +59,7 @@
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
-                                    </a>
+                                </a>
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
